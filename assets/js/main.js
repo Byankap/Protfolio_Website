ec79bcc1893064edaf64ef04b37e7517e7fe38de
Byanka Partlow <byanka.partlow@gmail.com>
2022-06-11 22:27:16 -0700

    updated files

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,11 +29,18 @@ const sections = document.querySelectorAll('section[id]')
 window.addEventListener('scroll', scrollActive)
 
 function scrollActive(){
-    const scrolly = window.pageYOffset
+    const scrollY = window.pageYOffset
 
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight
-        const section = current.offsetHeight
+        const sectionTop = current.offsetHeight - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop +sectionHeight){
+            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.remove('active')
+        }
     })
 
 
